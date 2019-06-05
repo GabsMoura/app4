04c40b5d7a8ed210f94c1e4f6f3c0d58f978467b
GABRIEL MOURA MORGANTE <gabriel.mmorgante@senacsp.edu.br>
2019-06-05 14:55:20 -0300

    Aplicativo para gerar números aleatórios

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+// Importando o React e o Componente.
 import React, {Component} from 'react';
+
+// Importando os elementos a serem utilizados.
 import {Button, Text, View, Alert} from 'react-native';
 
+// Criei uma função para gerar números aleatórios.
 const gerarNumeroAleatorio = () =>{
   let numero = Math.random();
   numero = Math.floor(numero * 10);
+  
+  // Alert é utilizado para exibir informações em forma de alerta.
   Alert.alert("Número Gerado",`${numero}`);
 }
 export default class App extends Component {
+  // BUTTON- Utilizando o onPress para chamar uma função quando tocar no botão com o nome "Gerar Número".
   render() {
     return (
       <View>
---
+++ readme.md
@@ -0,0 +1,5 @@
+# App4
+
+Aplicativo para gerar números aleatórios utilizando uma função randômica
+
+Criado por Gabriel Moura
